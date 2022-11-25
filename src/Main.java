b35c069fa5c9fe040b4c688ad12c97765061f123
Maciech <maciek.chodacki@wp.pl>
2022-11-25 16:03:57 +0100

    Few functions added with calculation of tax and duty costs based on predicted bid value

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello in Car Import Calculator");
+        System.out.println("Hello in Car Import Calculator \n");
         double kursDolara = 4.52;
-        System.out.println("Aktualny kurs dolara to: "+kursDolara);
+        System.out.println("Aktualny kurs dolara to: "+kursDolara +"\n");
         CarProperties carEvaluation = new CarProperties(1200,4200);
 
         System.out.println(carEvaluation.sumFees(carEvaluation));
@@ -22,6 +22,14 @@ public class Main {
             bidAmount = min;
         }
         return bidAmount;
+    }
+    private static double customDuty(CarProperties carProperties){
+        return carProperties.carBid*0.1;
+    }
+    //Tax 21% Calculation - 21% counter from car value plus the value of the duty
+    private static double tax21(CarProperties carProperties){
+        double result = carProperties.carBid + customDuty(carProperties);
+        return result*1.21;
     }
     private static void displayEstimatedValues(CarProperties carProperties){
         StringBuilder stringBuilder = new StringBuilder("\n" +"Costs estimation"+ "\n");
