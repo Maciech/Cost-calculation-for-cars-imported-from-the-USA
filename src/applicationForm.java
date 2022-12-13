31a33cbc0fa52cdecd339c6e529ea883ff454480
Maciech <maciek.chodacki@wp.pl>
2022-12-13 10:01:36 +0100

    Application form added

---
+++ src/applicationForm.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class applicationForm {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JCheckBox checkBox1;
+}
